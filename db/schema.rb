6db85ef1f8bb3f01191a3ab428136565d15833a0
ACLTC <acltc@ACLTCs-MacBook-Air.local>
2015-12-17 18:35:54 -0800

    finished git commands feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151218021529) do
+
+  create_table "git_commands", force: :cascade do |t|
+    t.string   "command"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
